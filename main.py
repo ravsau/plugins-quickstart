ce02a0e4f7abcc178cb85adf0a6372125b0f9647
ravsau <sauravsharma011@gmail.com>
2023-07-02 13:17:01 -0400

    add joke plugin example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import aiohttp
 
 import quart
 import quart_cors
@@ -6,29 +7,18 @@ from quart import request
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
-
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
-
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
-
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.get("/jokes/<string:username>")
+async def get_joke(username):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit') as resp:
+            joke_data = await resp.json()
+            if joke_data["type"] == "twopart":
+                joke = f'{joke_data["setup"]} {joke_data["delivery"]}'
+            else:
+                joke = joke_data["joke"]
+            
+            print( f'joke is:{joke}')
+            return quart.Response(response=json.dumps({"joke": joke}), status=200)
 
 @app.get("/logo.png")
 async def plugin_logo():
